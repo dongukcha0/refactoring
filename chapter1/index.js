f69b317440bc358173b50d17eff4fb27c24eff81
donguk <dongukcha0@gmail.com>
2019-05-26 20:27:16 +0900

    -

---
+++ chapter1/index.js
@@ -0,0 +1,16 @@
+// var hw = document.getElementById('hw');
+// hw.addEventListener('click', function(){
+//     alert('Hello world');
+// })
+
+// function byTwo(number){
+//     return number*2;
+// }
+//
+// console.log(byTwo(2))
+
+// function byTwo(number){
+//     return number << 1;
+// }
+//
+// console.log(byTwo(2))
---
+++ chapter3/index.js
@@ -0,0 +1,15 @@
+// var checkHand = function(hand){
+//     if(checkStraightFlush(hand)){
+//         return 'straight flush';
+//     } else if(checkFourOfKind(hand)){
+//         return 'four of a kind'
+//     } else if(checkFullHouse(hand)){
+//         return 'full house'
+//     } else if(checkPair(hand)){
+//         return 'pair'
+//     } else {
+//         return 'high card'
+//     }
+// }
+//
+// console.log(checkHand(['2-H', '3-C', '4-D', 'S-H', '2-C']))
